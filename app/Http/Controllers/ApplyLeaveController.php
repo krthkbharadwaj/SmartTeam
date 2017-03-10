8caa8a1bc250bab662c066d17dcef172a1908a72
Karthik Bharadwaj <krthkbharadwaj@gmail.com>
2017-03-10 16:35:09 +0530

    dev Initial commit to Laravel project

---
+++ app/Http/Controllers/ApplyLeaveController.php
@@ -0,0 +1,13 @@
+<?php
+class ApplyLeaveController extends Controller
+{
+	public function index()
+	{
+		return View::make('apply-leave');
+	}
+        public function store()
+	{
+		print_r("hhh");exit;
+    	}
+}
+?>
